68cb9a7f1c03c6bec1ca293549a96424e42bf6a1
Richard L Zarth III <rlziii@icloud.com>
2022-04-18 18:11:55 -0400

    Empty Swift.playground.

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -2,12 +2,3 @@ import Combine
 import Foundation
 import SwiftUI
 
-@available(iOS 15.0, *)
-func someFunc() {
-    print(#function)
-}
-
-someFunc()
-
-let a = Text("")
-    .gesture(TapGesture().simultaneously(with: TapGesture()).onEnded { _ in print("Hello, world!") })
